f07ba4923e4501e9a8b57b35b5380e6f50124e26
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-02-24 21:12:56 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/swproject/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/swproject/workshopmongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.swproject.workshopmongodb.domain.Post;
 import com.swproject.workshopmongodb.domain.User;
+import com.swproject.workshopmongodb.dto.AuthorDTO;
 import com.swproject.workshopmongodb.repository.PostRepository;
 import com.swproject.workshopmongodb.repository.UserRepositoty;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Patiu viagem", "Vou viajar para ão Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userRepositoty.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Patiu viagem", "Vou viajar para ão Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
