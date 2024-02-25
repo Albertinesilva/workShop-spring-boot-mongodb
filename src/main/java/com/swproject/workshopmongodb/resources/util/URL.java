f8a49943ff51efba25d94d68ff7eec2e16126e3a
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-02-25 01:23:16 -0300

    Consulta com vários critérios

--- src/main/java/com/swproject/workshopmongodb/resources/util/URL.java
+++ src/main/java/com/swproject/workshopmongodb/resources/util/URL.java
@@ -12,6 +12,7 @@ public class URL {
 	public static String decodeParam(String text) {
 		try {
 			return URLDecoder.decode(text, "UTF-8");
+			
 		} catch (UnsupportedEncodingException e) {
 			return "";
 		}
@@ -22,6 +23,7 @@ public class URL {
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		try {
 			return sdf.parse(textDate);
+			
 		} catch (ParseException e) {
 			return defaultValue;
 		}
